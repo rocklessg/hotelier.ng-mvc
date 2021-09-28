5fe417be4754ab332e681d30b8079f89420c75c9
adeola1976 <adeolavictor618@gmail.com>
2021-09-28 17:19:56 +0100

    login and sign up features(pages) first commit

---
+++ hotel-booking-mvc/Controllers/Auth/SignUpController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hotel_booking_mvc.Controllers.Auth
+{
+    public class SignUpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
