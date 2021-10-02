dfb8d80badb2e0ccd7e986786139dfe87241492a
Honoura Michael <honoura.michael@decagon.dev>
2021-10-02 20:37:56 +0100

    Commit to pull

---
+++ hotel-booking-mvc/Controllers/AdminDashboardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hotel_booking_mvc.Controllers
+{
+    public class AdminDashboardController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
