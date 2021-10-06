589e1aed9985e485f9f4777cc2d186dea303bcad
Honoura Michael <honoura.michael@decagon.dev>
2021-10-06 14:54:20 +0100

    Commit to pull nav bar

--- hotel-booking-mvc/Controllers/AdminDashboardController.cs
+++ hotel-booking-mvc/Controllers/AdminDashboardController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using hotel_booking_model;
+using hotel_booking_services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +13,48 @@ namespace hotel_booking_mvc.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            TransactionPeriod transactionPeriod = new TransactionPeriod();
+            //ViewData["transactionPeriod"] = transactionPeriod;
+            return View(transactionPeriod);
         }
     }
 }
+
+//private readonly IBook _adminBook;
+//private readonly IUser _adminUser;
+//private readonly IMainCategory _category;
+//private readonly ISubCategory _subCategory;
+//private readonly IRating _rating;
+//private readonly IReview _review;
+
+//public AdminController(
+//    IBook adminBook,
+//    IUser adminUser,
+//    IMainCategory category,
+//    ISubCategory subCategory,
+//    IRating rating,
+//    IReview review
+
+//    )
+//{
+//    _adminUser = adminUser;
+//    _adminBook = adminBook;
+//    _category = category;
+//    _subCategory = subCategory;
+//    _rating = rating;
+//    _review = review;
+//}
+
+//public IActionResult Dashboard()
+//{
+//    var users = _adminUser.GetAllUsers();
+//    ViewData["AllUsers"] = users;
+//    var books = _adminBook.GetAllBooks();
+//    ViewData["AllBooks"] = books;
+//    var usersCount = users.Count;
+//    var booksCount = books.Count;
+
+//    ViewData["usersCount"] = usersCount;
+//    ViewData["booksCount"] = booksCount;
+//    return View();
+//}
