cf2885f10b110a6bec6ed5084e872e08f6ca34c1
faivegid <49518762+faivegid@users.noreply.github.com>
2021-10-13 20:04:46 +0100

    created hotel list page

---
+++ hotel-booking-model/HotelBasicView.cs
@@ -0,0 +1,16 @@
+﻿namespace hotel_booking_model
+{
+    public class HotelBasicView
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public double Rating { get; set; }
+        public int NumberOFRatings { get; set; }
+        public string Comment { get => (Rating > 7) ? "Good" : "Average"; }
+        public string FeaturedImage { get; set; }
+    }
+}
--- hotel-booking-mvc/Controllers/AdminController.cs
+++ hotel-booking-mvc/Controllers/AdminController.cs
@@ -1,9 +1,6 @@
 ﻿using hotel_booking_model;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace hotel_booking_mvc.Controllers.Admin
 {
@@ -24,6 +21,28 @@ namespace hotel_booking_mvc.Controllers.Admin
         public IActionResult SingleRoom()
         {
             return View("../Hotel/SingleRoom");
+        }
+
+        public IActionResult AllHotels(int pageNumber)
+        {
+            var hotel = new HotelBasicView
+            {
+                Id = "Favelewkew",
+                Rating = 4.5,
+                Name = "The Croniche Hotel Bistro",
+                Description = "Offering an outdoor pool and a restaurant, Sheraton Lagos Hotel is located in Lagos. Free WiFi access is available. Each room here will provide you with a TV, air conditioning and a seating area.",
+                FeaturedImage = "https://s3-alpha-sig.figma.com/img/2b59/1574/6b2c85ee661a6c49cb920c969ad4c0c0?Expires=1634515200&Signature=K8kRMAa2d6ySoD1XJcIR0OSGx3o-hCCMR~VZZ~9ppVyjJLUVf0mAJydp4UAlaAk1AO6LUxxYCxHmguxYt4yKyuw-biREhgRMae7Dj4p3VuDj2ZUrWuTLshK5MH-teTG-X0J3tS-7sD6Iu6BmWrrOSrgLF4XcDrUMYFdmFjWmt2~-0E6lghkwa8J~VMq7qmQzmsf3mn3ZTvwMWtItMKj2mL0QIsV~7CyWcuFxG-by41typSXut4yLOkXGJkMc1xetuIZcUfRS1HBnAFU8sDFn~R7p83bavKKpRBNJX1EH2k5DHCEfi1xU9cNjuZ7zL6C2dGZftKvQk1PRajROWm3l9w__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+                Address = "Abel Oreniyi street off Salvation Road Opebi",
+                State ="Lagos",
+                City ="IKeja",
+                NumberOFRatings = 209                
+            };
+            var hotelList = new List<HotelBasicView>();
+            for (int i = 0; i < 5; i++)
+            {
+                hotelList.Add(hotel);
+            }
+            return View("../hotel/allhotels",hotelList);
         }
     }
 }
