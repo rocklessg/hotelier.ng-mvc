737141b6349f8e2b623be631bb630074d9dc8e90
obinna asiegbulam <obinna.asiegbulam@decagon.dev>
2021-10-24 21:17:37 +0100

    Access token added to Login

--- hotel-booking-services/Implementations/AuthenticationService.cs
+++ hotel-booking-services/Implementations/AuthenticationService.cs
@@ -1,5 +1,6 @@
 using hotel_booking_model.commons;
 using hotel_booking_model.Dtos.AuthenticationDtos;
+using hotel_booking_model.ViewModels;
 using hotel_booking_services.Interfaces;
 using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
@@ -9,17 +10,19 @@ namespace hotel_booking_services.Implmentations
     public class AuthenticationService : IAuthenticationService
     {
         private readonly IHttpRequestFactory _httpRequestFactory;
-        
-        public AuthenticationService(IHttpRequestFactory httpRequestFactory)
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public AuthenticationService(IHttpRequestFactory httpRequestFactory, IHttpContextAccessor httpContextAccessor)
         {
             _httpRequestFactory = httpRequestFactory;
-            
+            _httpContextAccessor = httpContextAccessor;
         }
 
-        public async Task<LoginResponseDto> Login(LoginDto loginDto)
+        public async Task<LoginViewModel> Login(LoginDto loginDto)
         {
 
-            var result = await _httpRequestFactory.PostRequestAsync<LoginDto, BasicResponse<LoginResponseDto>>("/api/Authentication/login", loginDto) ;
+            var result = await _httpRequestFactory.PostRequestAsync<LoginDto, BasicResponse<LoginViewModel>>("/api/Authentication/login", loginDto);
+            _httpContextAccessor.HttpContext.Session.SetString("access_token", result.Data.Token);
             return result.Data;
         }
 
@@ -29,7 +32,5 @@ namespace hotel_booking_services.Implmentations
             var result = await _httpRequestFactory.PostRequestAsync<RegisterDto, BasicResponse<RegisterDto>>("/api/Authentiation/register", registerdto);
             return result.Data;
         }
-        
-
     }
 }
--- hotel-booking-services/Interfaces/IAuthenticationService.cs
+++ hotel-booking-services/Interfaces/IAuthenticationService.cs
@@ -1,7 +1,5 @@
-using hotel_booking_model.commons;
 using hotel_booking_model.Dtos.AuthenticationDtos;
 using hotel_booking_model.ViewModels;
-using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
 
 namespace hotel_booking_services.Interfaces
@@ -9,9 +7,6 @@ namespace hotel_booking_services.Interfaces
     public interface IAuthenticationService
     {
         Task<LoginViewModel> Login(LoginDto loginDto);
-
-       // public SignupDto Signup(SignupModel signupModel);
-
         Task<RegisterDto> Register(RegisterDto registerdto);
     }
 }
