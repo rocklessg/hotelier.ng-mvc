d5c6c87d554a9d78265f003f5be7c410e09a73b0
obinna asiegbulam <obinna.asiegbulam@decagon.dev>
2021-10-25 11:04:09 +0100

    Change localhost to Heroku url

--- hotel-booking-services/Implementations/AdminService.cs
+++ hotel-booking-services/Implementations/AdminService.cs
@@ -22,7 +22,7 @@ namespace hotel_booking_services.Implmentations
             var response = await _httpRequestFactory.GetRequestAsync
                 <BasicResponse<AdminStatisticsDto>>(
                     requestUrl: $"api/Statistics/get-statistics/admin",
-                    baseUrl: "https://localhost:44319/");
+                    baseUrl: "http://hoteldotnet.herokuapp.com");
             return response.Data;
         }
 
