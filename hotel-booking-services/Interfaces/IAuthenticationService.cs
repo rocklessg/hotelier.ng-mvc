1131ff5e85c9bc772f64f2f6faaa33ce78c91ba7
Praise Balogun <praise.balogun@decagon.dev>
2021-10-27 09:16:43 +0100

    deleted the extra view model class

--- hotel-booking-services/Interfaces/IAuthenticationService.cs
+++ hotel-booking-services/Interfaces/IAuthenticationService.cs
@@ -2,13 +2,11 @@ using hotel_booking_model.commons;
 using hotel_booking_model.Dtos.AuthenticationDtos;
 using hotel_booking_model.ViewModels;
 using System.Threading.Tasks;
-using LoginViewModel = hotel_booking_model.Dtos.AuthenticationDtos.LoginViewModel;
 
 namespace hotel_booking_services.Interfaces
 {
     public interface IAuthenticationService
     {
-        //Task<hotel_booking_model.LoginViewModel> Login(LoginDto loginDto);
         Task<BasicResponse<LoginViewModel>> Login(LoginDto loginDto);
 
         Task<RegisterDto> Register(RegisterDto registerdto);
