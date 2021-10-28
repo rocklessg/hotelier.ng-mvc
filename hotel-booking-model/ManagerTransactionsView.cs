1049cbef2f61e17be6fe2c6d536a5e3c0cd7e35d
tm-laurenzo <Chibuike.Chidolue@decagon.dev>
2021-10-28 15:49:29 +0100

    created GetAllManagerTransactionsAsync method to the Manager Service, also added ManagerTransactionView to the Model

---
+++ hotel-booking-model/ManagerTransactionsView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotel_booking_model
+{
+    public class ManagerTransactionsView
+    {
+        public string Name { get; set; }
+        public string ServiceName { get; set; }
+        public string BookingReference { get; set; }
+        public bool Status { get; set; }
+        public double Amount { get; set; }
+        public double Commission { get; set; } 
+       
+    }
+}
--- hotel-booking-services/Implementations/ManagerService.cs
+++ hotel-booking-services/Implementations/ManagerService.cs
@@ -1,4 +1,6 @@
-﻿using hotel_booking_services.Interfaces;
+﻿using hotel_booking_model;
+using hotel_booking_model.commons;
+using hotel_booking_services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +15,15 @@ namespace hotel_booking_services.Implmentations
         public ManagerService(IHttpRequestFactory httpRequestFactory)
         {
             _httpRequestFactory = httpRequestFactory;
+        }
+
+        public async Task<IEnumerable<ManagerTransactionsView>> GetAllManagerTransactionsAsync(string managerId)
+        {
+            var response = await _httpRequestFactory.GetRequestAsync
+                <BasicResponse<IEnumerable<ManagerTransactionsView>>>(
+                    requestUrl: $"api/Manager/{managerId}/hotels", // change this line to connect to the right api endpoint
+                    baseUrl: "http://hoteldotnet.herokuapp.com");
+            return response.Data;
         }
 
     }
