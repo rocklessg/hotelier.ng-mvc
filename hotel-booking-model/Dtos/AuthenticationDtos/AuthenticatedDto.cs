3fa1a393aff0af167975adc0edf89a1e35dd113f
enabsdigital@gmail.com <happy.enabulele.decagon.dev>
2021-10-29 14:07:04 +0100

    commits

---
+++ hotel-booking-model/Dtos/AuthenticationDtos/AuthenticatedDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotel_booking_model.Dtos.AuthenticationDtos
+{
+    public class AuthenticatedDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Avatar { get; set; }
+        public string FullName { 
+            get { return FirstName + " " +  LastName; }
+        }
+    }
+}
--- hotel-booking-mvc/Controllers/AuthController.cs
+++ hotel-booking-mvc/Controllers/AuthController.cs
@@ -1,9 +1,13 @@
 using hotel_booking_services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using hotel_booking_model.Dtos.AuthenticationDtos;
 using System.Threading.Tasks;
-
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using System.Collections;
 
 namespace hotel_booking_mvc.Controllers.Auth
 {
@@ -28,15 +32,20 @@ namespace hotel_booking_mvc.Controllers.Auth
             {
                 var response = await _auth.Login(loginDto);
                 var result = response.Data;
-
-                if (result == null)
+                var handler = new JwtSecurityTokenHandler();
+                JwtSecurityToken decodedValue = handler.ReadJwtToken(result.Token);
+                Hashtable user = new Hashtable();
+                user.Add("FirstName", decodedValue.Claims.ElementAt(2).Value);
+                user.Add("LastName", decodedValue.Claims.ElementAt(3).Value);
+                user.Add("Avatar", decodedValue.Claims.ElementAt(4).Value);
+                var Role = decodedValue.Claims.ElementAt(5).Value;
+                HttpContext.Session.SetString("User", JsonConvert.SerializeObject(user));
+                if (Role == null)
                 {
                     ModelState.AddModelError(string.Empty, "Invalid Login Details");
                     return View();
                 }
-
-                role = result.Claim.Value;
-                if (role == "Manager")
+                else if (Role == "Manager")
                 {
                     return RedirectToAction("Dashboard", "Manager");
                 }
@@ -88,6 +97,12 @@ namespace hotel_booking_mvc.Controllers.Auth
         public IActionResult ResetPassword()
         {
             return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Auth", "Login");
         }
     }
 }
