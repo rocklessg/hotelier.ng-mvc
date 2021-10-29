638bccd8c38452da3b0024398b39e6046524cf12
enabsdigital@gmail.com <happy.enabulele.decagon.dev>
2021-10-29 17:32:16 +0100

    Added Id to Authenticated Model

--- hotel-booking-model/Dtos/AuthenticationDtos/AuthenticatedDto.cs
+++ hotel-booking-model/Dtos/AuthenticationDtos/AuthenticatedDto.cs
@@ -8,6 +8,7 @@ namespace hotel_booking_model.Dtos.AuthenticationDtos
 {
     public class AuthenticatedDto
     {
+        public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Avatar { get; set; }
